e0020359af05531fc27d8cbdcb597b79eb0922e0
William isaque <williamisaque18@gmail.com>
2021-11-24 00:42:31 -0300

    nova

--- index.js
+++ index.js
@@ -2,10 +2,217 @@ const express = require("express");
 const app = express();
 const axios = require("axios");
 const cors = require("cors");
+const { json } = require("express");
 app.use(express.json());
 require("dotenv").config();
-app.get("/", (req, res) => {
-  res.send("seja bem vindo");
+app.get("/", async (req, res) => {
+  console.log("iniciad");
+  let results = await axios.get(
+    "https://maps.googleapis.com/maps/api/place/textsearch/json?location=-23.0101811%2C-45.5583074&query=cardiologista&key=AIzaSyA1p_fKTIVgZJ2yF5FkJEEE0uGp-Jo7nnI"
+  );
+  //console.log(results.data);
+  //res.send("seja bem vindo");
+  let resultado = [];
+  const siglasEstados = [
+    "AC",
+    "AL",
+    "AP",
+    "AM",
+    "BA",
+    "CE",
+    "DF",
+    "ES",
+    "GO",
+    "MA",
+    "MT",
+    "MS",
+    "MG",
+    "PA",
+    "PB",
+    "PR",
+    "PE",
+    "PI",
+    "RJ",
+    "RN",
+    "RS",
+    "RO",
+    "RR",
+    "SC",
+    "SP",
+    "SE",
+    "TO",
+  ];
+  for (let element = 0; element < results.data.results.length; element++) {
+    let base = results.data.results[element].formatted_address;
+    let end = results.data.results[element].formatted_address.split(",");
+    let en = "erro";
+    let rua = "erro";
+    let n = "erro";
+    let bairro = "erro";
+
+    if (end[0].slice(0, 1).includes("R")) {
+      if (
+        base.indexOf("sala") == -1 &&
+        (base.indexOf("Andar") == -1 || base.indexOf("andar") == -1)
+      ) {
+        if (end[0].slice(0, 2) == "R.") {
+          //console.log(end[0].slice(0, 2) + "|" + end[0]);
+          //end[1].indexOf("-") != -1
+
+          // if(end[1].slice(end[1].indexOf("-") + 1, end[1].length).trim().slice(0,1))
+          var reg = /[A-Z]+/i;
+          bairro = reg.test(n);
+          console.log(reg.test(n));
+          rua = end[0];
+          n = end[1].slice(0, end[1].indexOf("-")).trim();
+          bairro = end[1].slice(end[1].indexOf("-") + 1, end[1].length).trim();
+        } else {
+          console.log(
+            "algo tem .",
+            end[2].slice(0, end[2].indexOf("-")).trim()
+          );
+          if (base.includes("R.") && base.includes("R")) {
+            for (let index = 0; index < siglasEstados.length; index++) {
+              let aux = base.slice(
+                base.indexOf("-") + 1,
+                base.indexOf(siglasEstados[index])
+              );
+
+              if (base.indexOf(siglasEstados[index]) != -1) {
+                bairro = aux.slice(0, aux.indexOf(",")).trim();
+                let baseN = base.slice(
+                  base.indexOf(aux) - 10,
+                  base.indexOf("-")
+                );
+                console.log("base n", baseN);
+                console.log(baseN.slice(baseN.indexOf(",") + 1).trim());
+                rua = end[1];
+                n = baseN.slice(baseN.indexOf(",") + 1).trim();
+
+                console.log("aux", aux);
+                console.log("base ", base);
+              }
+            }
+          }
+        }
+        // console.log((base.indexOf("sala") != -1) + "|" + base);
+      } else if (
+        base.indexOf("sala") != -1 &&
+        (base.indexOf("andar") != -1 || base.indexOf("Andar") != -1)
+      ) {
+        console.log(
+          "$$$" +
+            base
+              .slice(base.indexOf("sala") + 8, base.indexOf("sala") + 9)
+              .includes("-")
+        );
+        if (
+          base
+            .slice(base.indexOf("sala") + 8, base.indexOf("sala") + 9)
+            .includes("-") == true
+        ) {
+          rua = end[0];
+          bairro = base
+            .slice(
+              base.indexOf("-", base.indexOf("sala")) + 1,
+              base.indexOf(",", base.indexOf("sala"))
+            )
+            .trim();
+          n = base.slice(base.indexOf(",") + 1, base.indexOf("-")).trim();
+          console.log(
+            "sala" +
+              results.data.results[element].name +
+              "|" +
+              base
+                .slice(
+                  base.indexOf("-", base.indexOf("sala")) + 1,
+                  base.indexOf(",", base.indexOf("sala"))
+                )
+                .trim()
+          );
+        } else if (
+          base
+            .slice(base.indexOf("sala") + 8, base.indexOf("sala") + 9)
+            .includes("") == true
+        ) {
+          rua = end[0];
+          bairro = base
+            .slice(
+              base.indexOf("sala") + 9,
+              base.indexOf("-", base.indexOf("sala"))
+            )
+            .trim();
+          console.log(
+            "sala" +
+              results.data.results[element].name +
+              "|" +
+              base
+                .slice(
+                  base.indexOf("sala") + 9,
+                  base.indexOf("-", base.indexOf("sala"))
+                )
+                .trim()
+          );
+          n = base.slice(base.indexOf(",") + 1, base.indexOf("-")).trim();
+        }
+      }
+
+      // en = "rua";
+    } else if (end[0].slice(0, 3).includes("Av.")) {
+      for (let index = 0; index < siglasEstados.length; index++) {
+        let aux = base.slice(
+          base.indexOf("-") + 1,
+          base.indexOf(siglasEstados[index])
+        );
+
+        if (base.indexOf(siglasEstados[index]) != -1) {
+          if (aux.indexOf("sala") != -1) {
+            console.log(
+              "numero",
+              aux.slice(aux.indexOf("-") + 1, aux.indexOf(",")).trim()
+            );
+            rua = end[0];
+            bairro = aux.slice(aux.indexOf("-") + 1, aux.indexOf(",")).trim();
+            n = end[1].slice(0, 4).trim();
+          } else {
+            console.log(
+              "auxiliar avenida",
+              base
+                .slice(base.indexOf(aux) - 6, base.indexOf(aux) - 1)
+                .replace(",", "")
+                .trim()
+            );
+            bairro = aux.slice(0, aux.indexOf(",")).trim();
+            n = base
+              .slice(base.indexOf(aux) - 6, base.indexOf(aux) - 1)
+              .replace(",", "")
+              .trim();
+            rua = end[0];
+          }
+        }
+
+        en = "avenida";
+      }
+    } else if (
+      end[0].slice(0, 1).includes("A") == false &&
+      end[0].slice(0, 1).includes("R") == false
+    ) {
+      en = "numero";
+    }
+    //isNaN(end[0].slice(0, 1) )
+
+    resultado.push({
+      nomeFantasia: results.data.results[element].name,
+      avaliação: results.data.results[element].rating,
+      endereco: results.data.results[element].formatted_address, //end[2].indexOf("^[0-9]/g"),
+      latitude: results.data.results[element].geometry.location.lat,
+      longitude: results.data.results[element].geometry.location.lng,
+      rua: rua,
+      numero: n,
+      bairro: bairro,
+    });
+  }
+  res.status(200).json(resultado);
 });
 app.post("/cordenadas", async (req, res) => {
   console.time("tempo");
@@ -20,7 +227,7 @@ app.post("/cordenadas", async (req, res) => {
     );
 
     arraydeinformacoes = [...informacao.data];
-    // console.log(arraydeinformacoes) ;
+    // console.log(arraydeinformacoes);
     for (let i = 0; i < arraydeinformacoes.length; i++) {
       //console.log(arraydeinformacoes[i]);
       if (
@@ -57,6 +264,7 @@ app.post("/cordenadas", async (req, res) => {
 
       return data;
     }
+
     // console.log(sequencia(endereco.length));
     const request = sequencia(endereco.length).map(loadData);
 
@@ -86,6 +294,7 @@ app.post("/cordenadas", async (req, res) => {
           .send(`erro ocorrido durante a solicitação dos dados:\n ${err}`)
       );
   } catch (err) {
+    console.log(err);
     res.status(403).send(`erro ocorrido durante a solicao dos dados:\n ${err}`);
   }
 });
