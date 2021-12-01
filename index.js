ecfffbe324956d7ac396e2d1a72d8d6b9e6eb1c3
William isaque <williamisaque18@gmail.com>
2021-12-01 13:18:22 -0300

    atualizacao

--- index.js
+++ index.js
@@ -780,7 +780,7 @@ app.post("/cordenadas", async (req, res) => {
       );*/
       data.push(
         axios.get(
-          `https://maps.googleapis.com/maps/api/directions/json?origin=${latitude},${longitude}&destination=${latitude},${longitude}&key=${process.env.GOOGLE_API_KEY}`
+          `https://maps.googleapis.com/maps/api/directions/json?origin=${latitude},${longitude}&destination=${resultado[element].lat},${resultado[element].long}&key=${process.env.GOOGLE_API_KEY}`
         )
       );
     }
