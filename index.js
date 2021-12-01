3faa7d09378d31495ea25e3b475bf8f0e957ba5b
William isaque <williamisaque18@gmail.com>
2021-12-01 17:20:28 -0300

    atualizacao final

--- index.js
+++ index.js
@@ -795,7 +795,7 @@ app.post("/cordenadas", async (req, res) => {
         )
       );
       console.log(
-        `${resultado[0].lat},${resultado[0].long},${latitude},${longitude}`
+        ` nome: ${resultado[element].name}${resultado[0].lat},${resultado[0].long},${latitude},${longitude}`
       );
     }
     console.log("sdss0", data);
