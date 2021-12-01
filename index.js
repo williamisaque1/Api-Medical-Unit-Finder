38b42cc759ceba3526aa9d94384f592ef5f16382
William isaque <williamisaque18@gmail.com>
2021-11-30 22:54:43 -0300

    atualizacao final

--- index.js
+++ index.js
@@ -792,7 +792,7 @@ app.post("/cordenadas", async (req, res) => {
   Promise.all(await loadData())
     .then((value) => {
       console.log("places  ", value.length);
-
+      console.log("km", km);
       value.forEach((element, index) => {
         //  console.log(element.data.routes[0].legs[0].distance);
         if (km * 1000 >= element.data.routes[0].legs[0].distance.value) {
@@ -832,7 +832,7 @@ app.post("/cordenadas", async (req, res) => {
       });
     })
     .catch((e) => {
-      res.status(404).send("erro");
+      res.status(401).send("erro");
     });
 });
 /*
